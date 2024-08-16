b9c6b468852a2f6d3e7c401cc263550c3fb27774
Elroro23 <rodolfodalessandro23@gmail.com>
2024-08-16 21:55:53 +0000

    Proyecto realizado con sus respectivos pasos.

--- src/models.py
+++ src/models.py
@@ -1,31 +1,53 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, CheckConstraint, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    user_id = Column(Integer, primary_key=True) 
+    username = Column(String(15), nullable=False, unique=True)
+    firstname = Column(String(20), nullable=False)
+    lastname = Column(String(20), nullable=False)
+    email = Column(String(30), nullable=False, unique=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Follower(Base):
+    __tablename__ = 'follower'
+#Convertimos user_from_id y user_to_id en llaves primarias(compuestas) para evitar que un usuario siga a otro más de una vez.
+#A la vez son llaves foráneas para poder relacionarlas con la tabla "user".
+    user_from_id = Column(Integer, ForeignKey('User.user_id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('User.user_id'), primary_key=True)
+#Esta línea asegura que user_from_id y user_to_id no sean iguales(Que un usuario no se siga asi mismo).
+#CheckConstraint hay que importarlo.
+    __table_args__ = (CheckConstraint('user_from_id <> user_to_id', name='check_user_ids_not_equal'),)
 
-    def to_dict(self):
+class Media(Base):
+    __tablename__ = 'media'
+    media_id = Column(Integer, primary_key=True)
+    #Define un tipo Enum con los valores 'image', 'video' y 'audio'. El parámetro name especifica el nombre del tipo enum en la base de datos.
+    #Definimos Enum con uno de los siguientes valores 'image, video audio'
+    #Y Le damos un nombre al conjunto de valores (name= name_type).
+    type = Column(Enum('image', 'video', 'image', name='media_type'), nullable=False)
+    url = Column(String, unique=True)
+    post_id = Column(Integer, ForeignKey('Post.user_id'))
+
+class Post(Base):
+     __tablename__ = 'post'
+     post_id = Column(Integer, primary_key=True)
+     user_id = Column(Integer, ForeignKey('User.user.id'))
+
+class Comment(Base):
+     __tablename__ = 'comment'
+     comment_id = Column(Integer, primary_key=True)
+     comment_text = Column(String(200))
+     author_id = Column(Integer, ForeignKey('User.user_id'))
+     post_id = Column(Integer, ForeignKey('Post.post_id'))
+
+def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
